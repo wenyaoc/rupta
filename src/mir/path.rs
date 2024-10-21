9cd64c892b06a4c7affd793679d3911d6500b94a
Wenyao Chen <wenyao.chen@unsw.edu.au>
2024-10-21 15:36:48 +1100

    count ref

--- src/mir/path.rs
+++ src/mir/path.rs
@@ -47,29 +47,29 @@ impl Path {
         }
     }
 
-    // pub fn is_parameter(&self) -> bool {
-    //     match &self.value {
-    //         PathEnum::Parameter { .. } => true,
-    //         PathEnum::QualifiedPath { base, .. } => base.is_parameter(),
-    //         _ => false,
-    //     }    
-    // }  
+    pub fn is_parameter(&self) -> bool {
+        match &self.value {
+            PathEnum::Parameter { .. } => true,
+            PathEnum::QualifiedPath { base, .. } => base.is_parameter(),
+            _ => false,
+        }    
+    }  
     
-    // pub fn is_call_return(&self) -> bool {
-    //     match &self.value {
-    //         PathEnum::ReturnValue { .. } => true,
-    //         PathEnum::QualifiedPath { base, .. } => base.is_call_return(),
-    //         _ => false,
-    //     }
-    // }
+    pub fn is_call_return(&self) -> bool {
+        match &self.value {
+            PathEnum::ReturnValue { .. } => true,
+            PathEnum::QualifiedPath { base, .. } => base.is_call_return(),
+            _ => false,
+        }
+    }
 
-    // pub fn get_parameter_ordinal(&self) -> Option<usize> {
-    //     match &self.value {
-    //         PathEnum::Parameter { ordinal, .. } => Some(*ordinal),
-    //         PathEnum::QualifiedPath { base, .. } => base.get_parameter_ordinal(),
-    //         _ => None,
-    //     }
-    // }
+    pub fn get_parameter_ordinal(&self) -> Option<usize> {
+        match &self.value {
+            PathEnum::Parameter { ordinal, .. } => Some(*ordinal),
+            PathEnum::QualifiedPath { base, .. } => base.get_parameter_ordinal(),
+            _ => None,
+        }
+    }
 }
 
 impl Debug for Path {
@@ -92,25 +92,25 @@ impl CSPath {
         })
     }
 
-    // pub fn get_context_id(&self) -> ContextId {
-    //     self.cid
-    // }
+    pub fn get_context_id(&self) -> ContextId {
+        self.cid
+    }
 
-    // pub fn get_cs_func_id(&self) -> Option<FuncId> {
-    //     self.path.get_func_id()
-    // }
+    pub fn get_cs_func_id(&self) -> Option<FuncId> {
+        self.path.get_func_id()
+    }
 
-    // pub fn is_cs_parameter(&self) -> bool {
-    //     self.path.is_parameter()
-    // }
+    pub fn is_cs_parameter(&self) -> bool {
+        self.path.is_parameter()
+    }
 
-    // pub fn get_parameter_ordinal(&self) -> Option<usize> {
-    //     self.path.get_parameter_ordinal()
-    // }
+    pub fn get_parameter_ordinal(&self) -> Option<usize> {
+        self.path.get_parameter_ordinal()
+    }
 
-    // pub fn is_cs_call_return(&self) -> bool {
-    //     self.path.is_call_return()
-    // }
+    pub fn is_cs_call_return(&self) -> bool {
+        self.path.is_call_return()
+    }
 }
 
 /// Different kinds of `Path` used in our analysis.
